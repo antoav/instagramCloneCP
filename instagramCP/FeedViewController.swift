e58c773fc263d6031fb5513e9f8b2650d021e86f
Antonella <avog001@fiu.edu>
2022-03-25 13:40:28 -0400

    fully working assignment

--- instagramCP/FeedViewController.swift
+++ instagramCP/FeedViewController.swift
@@ -10,8 +10,6 @@ import Parse
 import AlamofireImage
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-  
-
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -19,11 +17,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         tableView.delegate = self
         tableView.dataSource = self
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -65,14 +60,5 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         return cell
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
